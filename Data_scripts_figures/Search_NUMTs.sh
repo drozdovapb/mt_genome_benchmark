ce8b1b227e8612f26e69bfc10ae926fee84a32a6
MutinAndrei <98081107+MutinAndrei@users.noreply.github.com>
2026-08-10 13:39:10 +0800

    Delete Data_scripts_figures/Search_NUMTs.sh

--- Data_scripts_figures/Search_NUMTs.sh
+++
@@ -1,3 +0,0 @@
-# Поток: bwa mem -> samtools view (конвертация в BAM) -> samtools sort (сортировка) -> вывод в файл
-bwa mem -t 8 -a -M ../../../ref_mt_BM/circular.circular.16284.fasta ../Bpu_D1_trim_filt_1.fq ../Bpu_D1_trim_filt_2.fq 2> bwa.log | samtools view -bS - | samtools sort -@ 4 -T tmp.sort -o aligned.sorted_Bpul.bam -
-ls
